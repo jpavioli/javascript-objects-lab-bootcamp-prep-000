dfe461088a5d6cfbce022164d7f1de33aeab80a3
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-04 03:26:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 //deletes key from a clone of object and returns a new object (non distructive)
 function deleteFromObjectByKey(object,key){
   var newObj = {object}
-  delete newObj.key;
+  delete newObj[key]
   return newObj
 }
 
